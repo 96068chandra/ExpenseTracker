80de2bc43f8e794a873ce4fb9452f5e71a8d1061
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-11-06 23:44:46 +0530

    Add project files.

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+
+    }
+}
---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryID
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Description { get; set; }
+
+        public DateTime Date { get; set; }= DateTime.Now;
+
+    }
+}
