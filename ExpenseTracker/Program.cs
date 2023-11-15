3e29075c3a5470a980d9e9f0fa9cb8a747d71e97
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-11-15 16:07:03 +0530

    Adding Dashboard controller and Side bar

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
